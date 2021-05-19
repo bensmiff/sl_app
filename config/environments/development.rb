a06ad9a4731740a6f146bf703eebd13798366102
Ben Smith <bensmiff@hotmail.com>
2021-05-19 19:56:40 +0100

    Updating hosts for url access

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # BSTODO- Update this and maybeput as env variable?
+  config.hosts << "fc5d6c4.online-server.cloud"
 end
